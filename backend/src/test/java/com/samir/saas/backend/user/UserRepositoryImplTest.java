893c11011006378ffcb15bdfce6d91023183890c
samir <bettahar.samir@outlook.com>
2025-08-07 12:46:15 +0100

    test: added a simple save user test to make sure flyway migrations and connection to db works as excpected

---
+++ backend/src/test/java/com/samir/saas/backend/user/UserRepositoryImplTest.java
@@ -0,0 +1,52 @@
+package com.samir.saas.backend.user;
+
+import com.samir.saas.backend.user.domain.User;
+import com.samir.saas.backend.user.domain.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import com.samir.saas.backend.TestcontainersConfiguration;
+
+import java.time.Instant;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+@Transactional
+@Testcontainers
+@SpringBootTest
+@Import(TestcontainersConfiguration.class)
+class UserRepositoryImplTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void shouldSaveAndRetrieveUser() {
+        var id = UUID.randomUUID();
+        String email = "test@example.com";
+        String password = "secure";
+        Instant now = Instant.now();
+
+        User user = User.builder()
+                .id(id)
+                .email(email)
+                .password(password)
+                .createdAt(now)
+                .updatedAt(now)
+                .build();
+
+        userRepository.save(user);
+        System.out.println("User saved: " + user);
+
+        Optional<User> found = userRepository.findByEmail(email);
+
+        assertTrue(found.isPresent());
+        assertEquals(user.getId(), found.get().getId());
+        assertEquals(email, found.get().getEmail());
+        assertEquals(password, found.get().getPassword());
+    }
+}
